0e02338e557abad3a0a88eecae58b8a56233ec6b
John Mordecai <john.mordecai@gmail.com>
2021-09-18 00:54:38 -0400

    Clean up structure for CSS, JS, Other

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700|PT+Serif:400,400i,700&display=swap" rel="stylesheet">
 		<!-- font-family: 'Roboto', sans-serif; -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="styles.css">
+		<link rel="stylesheet" href="css/styles.css">
 
 	</head>
 	<body>	
@@ -147,6 +147,6 @@
 		<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-		<script src="scripts.js"></script>
+		<script src="js/scripts.js"></script>
 	</body>
 </html>
