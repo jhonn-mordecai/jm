4d5bf9d5baad43a772e03e34d87d6b6851f8a531
John Mordecai <john.mordecai@gmail.com>
2023-01-26 20:56:14 -0500

    Redesign 1st draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-$(document).ready(function(){
-
-    $(window).scroll(function(){
-        let scroll_trigger = $(window).scrollTop();
-
-        if (scroll_trigger > 650) {
-            $('header').slideDown();
-        } else {
-            $('header').slideUp();
-        }
-    });	
-    
-    // Scroll body to top when clicking .back-top
-    $('header h1 a').click(function() {
-        $('body,html').animate({scrollTop: 0}, 800);
-        return false;
-    });
-});
+/* Copyright Year */
+let copyYear = new Date().getFullYear();
+// document.querySelector(".copy_year").innerText = copyYear;
