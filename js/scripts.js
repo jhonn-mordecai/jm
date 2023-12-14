504c52a9b79b06761e67f3fd4f7e8c39f40b1154
John Mordecai <john.mordecai@gmail.com>
2023-12-13 20:04:44 -0500

    Initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,6 @@
-//Set dynamic scroll padding for nav
-function setScrollPadding() {
-    const navHeight = document.querySelector('header').offsetHeight;
-    document.documentElement.style.setProperty('--scroll-padding', navHeight - 1 + "px");
-}
-setScrollPadding();
-
-// Listens for window resize, in case height of navbar changes
-// NOT NEEDED RIGHT NOW
-// window.addEventListener("resize", setScrollPadding);
-
 //Copyright year
 const year = new Date().getFullYear();
 document.querySelector('.copyYear').textContent = year;
-
-//Toggle Nav Menu
-const toggleMenu = document.querySelector('.toggle-mobile-menu');
-toggleMenu.addEventListener('click', (e) => {
-    e.preventDefault();
-    
-    let icon = document.querySelector('.menu-toggle-icon');
-    let hasClass = icon.classList.contains('fa-bars');
-
-    if (hasClass) {
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-times');
-        toggleMenu.setAttribute('aria-expanded', 'true');
-    } else {
-        icon.classList.remove('fa-times');
-        icon.classList.add('fa-bars');
-        toggleMenu.setAttribute('aria-expanded', 'false');
-    }
-
-    document.querySelector('.nav-right').classList.toggle('toggle-flex');
-})
-
-//Add bottom shadow to header on scroll
-const header = document.querySelector('header');
-const summarySection = document.querySelector('.summary');
-
-window.addEventListener("scroll", () => {
-    let scrollPos = header.offsetTop;
-    let topOfSum = summarySection.offsetTop;
-
-    if (scrollPos > topOfSum - '130') {
-        header.classList.add('header-shadow');
-    } else {
-        header.classList.remove('header-shadow');
-    }
-})
 
 //Handle Email Form
 const submitEmail = document.getElementById("submitEmail");
@@ -66,3 +19,52 @@ submitEmail.addEventListener("click", (e) => {
     emailjs.send("service_ojlo0il", "template_8maqzcb", params);
     form.reset();
 })
+
+//Set dynamic scroll padding for nav
+// function setScrollPadding() {
+//     const navHeight = document.querySelector('header').offsetHeight;
+//     document.documentElement.style.setProperty('--scroll-padding', navHeight - 1 + "px");
+// }
+// setScrollPadding();
+
+// Listens for window resize, in case height of navbar changes
+// NOT NEEDED RIGHT NOW
+// window.addEventListener("resize", setScrollPadding);
+
+
+
+//Toggle Nav Menu
+// const toggleMenu = document.querySelector('.toggle-mobile-menu');
+// toggleMenu.addEventListener('click', (e) => {
+//     e.preventDefault();
+    
+//     let icon = document.querySelector('.menu-toggle-icon');
+//     let hasClass = icon.classList.contains('fa-bars');
+
+//     if (hasClass) {
+//         icon.classList.remove('fa-bars');
+//         icon.classList.add('fa-times');
+//         toggleMenu.setAttribute('aria-expanded', 'true');
+//     } else {
+//         icon.classList.remove('fa-times');
+//         icon.classList.add('fa-bars');
+//         toggleMenu.setAttribute('aria-expanded', 'false');
+//     }
+
+//     document.querySelector('.nav-right').classList.toggle('toggle-flex');
+// })
+
+//Add bottom shadow to header on scroll
+// const header = document.querySelector('header');
+// const summarySection = document.querySelector('.summary');
+
+// window.addEventListener("scroll", () => {
+//     let scrollPos = header.offsetTop;
+//     let topOfSum = summarySection.offsetTop;
+
+//     if (scrollPos > topOfSum - '130') {
+//         header.classList.add('header-shadow');
+//     } else {
+//         header.classList.remove('header-shadow');
+//     }
+// })
